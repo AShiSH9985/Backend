be5234a6d78daa4057bb907cd9a349ce9178bc75
Ashish <ashish8979778599@gmail.com>
2026-03-30 18:50:51 +0530

    backend complete

---
+++ moodify/Backend/src/models/song.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+
+const songSchema = new mongoose.Schema({
+    url:{
+        type:String,
+        required:true
+    },
+    postUrl:{
+        type:String,
+        required:true,
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    mood:{
+        type:String,
+        enum:{
+            values:["sad","happy","surprise"],
+            message:"enum this is"
+        }
+    }
+})
+
+const songModel = mongoose.model("songs",songSchema)
+
+module.exports = songModel
